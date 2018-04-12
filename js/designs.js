42fbf5c4e1c91c898b31b0481fcd141706512d9d
Ritu <rituraj@raj.com>
2018-04-12 19:05:46 +0530

    Changed UI, added brush and eraser feature

--- js/designs.js
+++ js/designs.js
@@ -2,7 +2,10 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-var color = "#27ae60";
+
+$(function() {
+  
+	var color = "#000";
 $("#colorPicker").val(color);
 
 function makeGrid() {
@@ -13,6 +16,7 @@ var width = $("#inputWeight").val();
 var table = $("#pixelCanvas");
 table.html("");
 
+//Makes a grid 
 for(var i = 0; i < height; i++){
     table.prepend("<tr></tr>");
 }
@@ -22,36 +26,93 @@ for(var j = 0; j < width; j++){
     $("tr").append("<td></td>");
 }
 
+//Changes color
 $("#colorPicker").on('change', function(){
     color = $(this).val();
 });
+
+//Apply color when clicked
+
 
 $('td').on("click",function(e){
     
 
 $(this).css("background-color",color);
-
-     
+    
 });
 
+//Remove color when double clicked
 $('td').on("dblclick",function(e){
     
      
-    $(this).css("background-color","white");
-    
-         
+    $(this).css("background-color","transparent");
+        
     });
+	
+	//Use brush feature
+	$('#use-brush').hover(function(e){
+		
+		$("#use-brush").addClass("add-brush");
+	});
+	  
+  $('#use-brush').on("click",function(e){
+	  
+	  e.preventDefault();
+	  
+	  $("body").addClass("add-brush");
+	  
+	  $("td").mouseover(function(e) {
+  
+		$(this).css("background-color",color);
+		});
+	
+  });
+	//Eraser feature
+  $('#reset-brush').hover(function(e){
+	
+	$("#reset-brush").removeClass("add-brush");
+	$("#reset-brush").addClass("add-eraser");
 
+  });
+	  $('#reset-brush').on("click",function(e){
+		  
+		  e.preventDefault();
+		  $("body").removeClass("add-brush");
+		  $("body").addClass("add-eraser");
+		$('td').mouseover(function(e){
+ 
+		$(this).css("background-color","transparent");
+
+
+		});
+
+		$('td').on("click",function(e){
+ 
+			$(this).css("background-color","transparent");
+	
+	
+			});
+  });
+	
+	
 }
+
+
+
 
 //Make a grid when user submit the button
 
 $("#submit").on("click",function(e){
   
 e.preventDefault();
+$("body").removeClass("add-eraser");
+$("body").removeClass("add-brush");
+$("body").addClass("add-normal");
 makeGrid();
    
     
 });
 
 
+});   
+ 
